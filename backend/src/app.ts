d764df8b09cd5447a8b25a6241b174af217840e1
diskoveri <vantaafm@club.fi>
2024-01-06 23:07:07 +0200

    Add backend folder

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+// src/app.ts
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
