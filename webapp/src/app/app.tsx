dcc96972afe4591da129fe121909774db718c589
diskoveri <vantaafm@club.fi>
2024-01-07 01:05:31 +0200

    Add some components & stylings

--- webapp/src/app/app.tsx
+++ webapp/src/app/app.tsx
@@ -1,9 +1,13 @@
 import { Component, h } from 'preact'
+import Header from '../components/header'
+import Hero from '../components/hero'
 
 export class App extends Component {
 	render() {
 		return (
 			<div>
+				<Header />
+				<Hero />
 				<p> terf</p>
 			</div>
 		)
