110e04aa4b639a53d937faf8e934f1adfdb42552
KillariDev <killari@gmail.com>
2024-01-20 16:17:37 +0200

    delete webapp and move to webapp_new

--- webapp/src/bootstrap.tsx
+++
@@ -1,6 +0,0 @@
-import { h, render } from 'preact'
-import { App } from './app/app'
-
-const rootElement = document.getElementById('app')
-if (rootElement === null) throw 'did not find app'
-render(<App/>, rootElement)
